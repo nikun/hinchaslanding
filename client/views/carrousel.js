9eaa143a19f9d636877e084b4befe4c17f185d39
elerman <ezequiel.lerman@gmail.com>
2013-06-06 18:22:27 -0300

    se agrega carousel

--- client/views/carrousel.js
+++ client/views/carrousel.js
@@ -3,3 +3,17 @@ Template.carousel.helpers({
 		return Users.find({});
 	}
 });
+
+Template.carousel.rendered = function(){
+	 $("#carousel").carouFredSel({
+        items               : 4,
+        direction           : "left",
+        scroll : {
+            items           : 1,
+            easing          : "elastic",
+            duration        : 1000,                        
+           pauseOnHover    : true
+        }                  
+    });
+
+}
