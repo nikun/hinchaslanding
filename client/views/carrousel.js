8b5ff6d095ef77b1d12784f3417d32f8a586dd75
elerman <ezequiel.lerman@gmail.com>
2013-06-07 17:48:24 -0300

    validaciones, popover validacion y mejora de carousel para efecto de diseño

--- client/views/carrousel.js
+++ client/views/carrousel.js
@@ -5,15 +5,47 @@ Template.carousel.helpers({
 });
 
 Template.carousel.rendered = function(){
-	 $("#carousel").carouFredSel({
-        items               : 5,
-        direction           : "left",
-        scroll : {
-            items           : 1,
-            easing          : "elastic",
-            duration        : 1000,                        
-           pauseOnHover    : true
-        }                  
-    });
+	$("#carousel").carouFredSel({
+		items               : 5,
+		direction           : "left",
+		responsive			: false,
+		scroll : {
+			items           : 1,
+			easing          : "elastic",
+			duration        : 1000,                        
+			pauseOnHover    : true
+		},
+		auto    : {
+			onBefore: function( data ) {
+				unhighlight( data.items.old );
+			},
+			onAfter : function( data ) {
+				highlight( data.items.visible );
+			}
+		}         
+	});
+
+	highlight( unhighlight( $("#carousel > *") ) );
+
+
+}
+
+function highlight( items ) {
+	items.filter(":eq(2)").css({
+		backgroundColor: "#ff9",
+		width   : 140,
+		height  : 140
+	});
+	return items;
+}
+
+function unhighlight( items ) {
+	items.css({
+		backgroundColor: "#fff",
+		width   : 100,
+		height  : 100
+	});
+
+	return items;
 
 }
